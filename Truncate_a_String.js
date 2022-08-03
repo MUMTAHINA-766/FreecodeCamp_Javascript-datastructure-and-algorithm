92a11da111d1b3bb52579dfe87fc3f79a6bf6cd6
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-08-03 10:41:01 +0600

    Update Truncate_a_String.js

--- Truncate_a_String.js
+++ Truncate_a_String.js
@@ -6,4 +6,23 @@ function truncateString(str, num) {
   return output;
 }
 
-let result = truncateString("A-tisket a-tasket A green and yellow basket", 8);
+//Another solution:
+// let result = truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+// function truncateString(str, num) {
+// //let emstr = "";
+// if (str.length === num || str.length < num){
+// return str
+// }else{
+// let emstr = str.slice(0,num);
+// return emstr+"..."
+// }
+
+
+// //return emstr+"..."
+
+// }
+
+// truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+// //truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length)
