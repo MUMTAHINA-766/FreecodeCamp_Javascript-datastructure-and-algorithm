a713927fc0218402c55de5dff445053020ee6820
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-08-06 09:07:59 +0600

    Update chunk_array.js

--- chunk_array.js
+++ chunk_array.js
@@ -14,3 +14,26 @@ function chunkArrayInGroups(arr, size) {
 }
 
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
+
+//2nd solution
+
+// function chunkArrayInGroups(arr, size) {
+//   let bigArray = [];
+//   let subArray = [];
+//   let subCntr = 0;
+//   for(let i = 0; i < arr.length; i++){
+//     if(subCntr === size){
+//       subCntr = 0;
+//       bigArray.push(subArray);
+//       subArray=[];
+//     }
+//      subArray.push(arr[i]);
+//      //console.log(subCntr,subArray);
+//     subCntr++;
+//   }
+//   bigArray.push(subArray)
+//   // console.log(bigArray);
+//   return bigArray;
+// }
+
+// chunkArrayInGroups(["a", "b", "c", "d","e","f","g"], 3);
