918ec42d7f0fe9bcd5722c66614a350f1f4900bc
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-08-17 09:59:18 +0600

    Update PigLatin.js

--- PigLatin.js
+++ PigLatin.js
@@ -1,30 +1,26 @@
 function translatePigLatin(str) {
-  let vow = ["a","e","i","o","u"] ;
+  let vow = "aeiou" ;
   let newStr = "";
  
     for(let i = 0;i<vow.length;i++){
-     if(str[0]==vow[i]){
-      //console.log("yes");
+     if(vow.includes(str[0])==true){
       newStr=str+"way";
+      return newStr;
     }
-    // else if(str[0]!=vow[i]){
-    //    //console.log("no");
-    //    newStr=str+str[0]+"ay";
-    //   newStr = newStr.substring(1);
-    // }
     }
-      for(let j = 0;j<vow.length;j++){
-     if(str[0]!=vow[j]){
-       //console.log("no");
-       newStr=str+str[0]+"ay";
-      newStr = newStr.substring(1);
-    }
+    let j = 0;
+    let cluster = "";
+    while(vow.includes(str[j])==false && j<str.length){
+    console.log(str[j]);
+    cluster+=str[j];
+    j++;
+    newStr = str.substring(j)+cluster+ "ay";
     }
   //console.log(str.length);
   //console.log(vow.length);
   console.log(str[0]);
-  //console.log(vow[0]);
+  console.log(cluster);
   console.log(newStr);
   return newStr;
 }
-translatePigLatin("algorithm");
+translatePigLatin("schwartz");
